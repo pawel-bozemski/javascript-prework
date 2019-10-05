add18285758819931865e6c5253792c88bc76f5c
Pawel Bozemski <paw.boz@gmail.com>
2019-10-05 18:21:09 +0200

    Add js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
 let playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!'); 
+
